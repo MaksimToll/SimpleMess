46080ae518da7015e7653ab25a06a55ee15d94f6
Maksim <maxim.tolstik@gmail.com>
2016-03-08 18:50:11 +0200

    asdfda

---
+++ src/main/java/com/simplemess/ConfigApp.java
@@ -0,0 +1,33 @@
+package com.simplemess;
+
+import com.simplemess.entity.User;
+import com.simplemess.repository.UserRespository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@SpringBootApplication
+
+public class ConfigApp {
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(ConfigApp.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(UserRespository respository) {
+        return (args) -> {
+            User user = new User("Anast", "Lot");
+            respository.save(user);
+            respository.save(new User("Maks", "Toll"));
+
+            for (User user1: respository.findAll()){
+                System.out.println( user.getName());
+            }
+        };
+    }
+}
